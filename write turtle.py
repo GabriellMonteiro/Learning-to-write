7093b86dfc85218396a3dbb0194302e80e69da93
Gabriel Monteiro <gabrielbrava@outlook.com.br>
2023-04-19 20:31:10 -0300

    Initial project

---
+++ write turtle.py
@@ -0,0 +1,394 @@
+import turtle
+
+from re import X
+
+turtle.bgcolor("green")
+turtle.color("white")
+turtle.title("Aprendendo a escrever!")
+turtle.speed(1)
+turtle.pensize(5)
+
+def a():
+    turtle.lt(65)
+    turtle.fd(100)
+    turtle.rt(130)
+    turtle.fd(100)
+    turtle.rt(180)
+    turtle.fd(50)
+    turtle.lt(65)
+    turtle.fd(50)
+
+def b():
+    turtle.rt(90)
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(20)
+    turtle.lt(20)
+    turtle.fd(150)
+    turtle.rt(90)
+    turtle.fd(130)
+    turtle.rt(90)
+    turtle.fd(130)
+    turtle.rt(20)
+    turtle.fd(25)
+    turtle.rt(20)
+    turtle.fd(25)
+    turtle.rt(20)
+    turtle.fd(25)
+    turtle.rt(20)
+    turtle.fd(25)
+    turtle.rt(20)
+    turtle.fd(23)
+    turtle.rt(20)
+    turtle.fd(18)
+    turtle.rt(20)
+    turtle.fd(20)
+    turtle.rt(20)
+    turtle.fd(25)
+    turtle.rt(20)
+    turtle.fd(100)
+
+
+def c():
+    turtle.lt(180)
+    turtle.fd(20)
+    turtle.circle(100, 180, )
+    turtle.fd(20)
+
+
+def d():
+    turtle.lt(180)
+    turtle.fd(60)
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.lt(90)
+    turtle.fd(60)
+    turtle.circle(75, 180, )
+
+
+def e():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.fd(50)
+    turtle.bk(50)
+    turtle.rt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.bk(50)
+    turtle.rt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(50)
+
+
+def f():
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.rt(90)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(75)
+
+
+def g():
+    turtle.lt(180)
+    turtle.fd(30)
+    turtle.circle(100, 180, )
+    turtle.fd(30)
+    turtle.lt(90)
+    turtle.fd(90)
+    turtle.lt(90)
+    turtle.fd(45)
+
+
+def h():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(200)
+    turtle.lt(180)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(200)
+
+
+def i():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.penup()
+    turtle.fd(20)
+    turtle.pendown()
+    turtle.fd(15)
+
+
+def j():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.rt(180)
+    turtle.fd(100)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.rt(90)
+    turtle.fd(120)
+    turtle.rt(90)
+    turtle.fd(50)
+
+
+def k():
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.lt(180)
+    turtle.fd(80)
+    turtle.lt(150)
+    turtle.fd(90)
+    turtle.lt(180)
+    turtle.fd(90)
+    turtle.lt(70)
+    turtle.fd(100)
+
+
+def l():
+    turtle.rt(90)
+    turtle.fd(150)
+    turtle.lt(90)
+    turtle.fd(90)
+
+
+def m():
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.rt(150)
+    turtle.fd(100)
+    turtle.lt(120)
+    turtle.fd(100)
+    turtle.rt(150)
+    turtle.fd(150)
+
+
+def n():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.rt(150)
+    turtle.fd(115)
+    turtle.lt(148)
+    turtle.fd(100)
+
+
+def o():
+    turtle.circle(80, 360, )
+
+
+def p():
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.rt(90)
+    turtle.fd(55)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(5)
+    turtle.rt(10)
+    turtle.fd(55)
+
+
+def q():
+    turtle.rt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(200)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(100)
+    turtle.lt(208)
+    turtle.fd(280)
+
+
+def r():
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.rt(90)
+    turtle.fd(70)
+    turtle.rt(90)
+    turtle.fd(40)
+    turtle.rt(90)
+    turtle.fd(70)
+    turtle.lt(140)
+    turtle.fd(90)
+
+
+def s():
+    turtle.lt(180)
+    turtle.fd(90)
+    turtle.lt(90)
+    turtle.fd(90)
+    turtle.lt(90)
+    turtle.fd(90)
+    turtle.lt(270)
+    turtle.fd(90)
+    turtle.lt(270)
+    turtle.fd(90)
+
+
+def t():
+    turtle.lt(90)
+    turtle.fd(150)
+    turtle.rt(90)
+    turtle.fd(90)
+    turtle.back(180)
+
+
+def u():
+    turtle.rt(90)
+    turtle.fd(120)
+    turtle.lt(90)
+    turtle.fd(90)
+    turtle.lt(90)
+    turtle.fd(120)
+
+
+def v():
+    turtle.lt(90)
+    turtle.lt(30)
+    turtle.fd(200)
+    turtle.lt(180)
+    turtle.fd(200)
+    turtle.rt(240)
+    turtle.fd(200)
+
+
+def x():
+    turtle.lt(45)
+    turtle.fd(200)
+    turtle.lt(180)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(180)
+    turtle.fd(200)
+
+
+def y():
+    turtle.lt(90)
+    turtle.rt(150)
+    turtle.fd(100)
+    turtle.lt(120)
+    turtle.fd(100)
+    turtle.rt(180)
+    turtle.fd(100)
+    turtle.lt(30)
+    turtle.fd(100)
+
+
+def w():
+    turtle.lt(90)
+    turtle.lt(30)
+    turtle.fd(200)
+    turtle.lt(180)
+    turtle.fd(200)
+    turtle.rt(240)
+    turtle.fd(200)
+    turtle.lt(240)
+    turtle.fd(200)
+    turtle.lt(120)
+    turtle.fd(200)
+
+
+def z():
+    turtle.lt(90)
+    turtle.rt(90)
+    turtle.fd(120)
+    turtle.rt(120)
+    turtle.fd(100)
+    turtle.rt(60)
+    turtle.fd(30)
+    turtle.rt(180)
+    turtle.fd(60)
+    turtle.rt(180)
+    turtle.fd(30)
+    turtle.rt(300)
+    turtle.fd(120)
+    turtle.rt(240)
+    turtle.fd(120)
+
+screen = turtle.Screen()
+screen.setup(500,500)
+while True:
+    sv = turtle.textinput("Turtle", "Escreva seu nome").lower()
+    X = locals()
+
+    for list in sv:
+        print(X[list]())
+        print(list)
+        turtle.clearscreen()
+        turtle.bgcolor("green")
+        turtle.color("white")
+        turtle.title("Aprendendo a escrever!")
+        turtle.speed(1)
+        turtle.pensize(5)
+        turtle.goto(0, 0)
